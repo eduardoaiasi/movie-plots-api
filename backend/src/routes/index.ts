155ef9095c022a76acae7e264b9cc74e90d572fa
eduardo <eduardo.diniz.aiasi@gmail.com>
2026-01-12 13:59:37 -0300

    feat: adiciona rota de health check para monitoramento do servidor

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -10,6 +10,7 @@
 
 import {Express, json} from 'express';
 import movieRoutes from './movieRoute'; // Importando o router de filmes
+import { uptime } from 'node:process';
 
 /**
  * Função que configura todas as rotas da aplicação
@@ -21,6 +22,16 @@ const routes = (app: Express) => {
     // Middleware para parsear JSON nas requisições
     // Permite que o Express entenda automaticamente o corpo das requisições em JSON
     app.use(json());
+
+    //Rota HEALTH CHECK, para verificar se o servidor está ativo, monitoramento e deploy em produção.
+    app.get("/health", (_req, res) => {
+        res.status(200).json({ 
+            status: "OK",
+            service: "movie-plots-api",
+            timestamp: new Date().toISOString(), // Data e hora atuais em formato ISO
+            uptime: uptime() // Tempo em segundos desde que o processo foi iniciado 
+        });
+    });
 
     // Rota raiz da aplicação - endpoint de teste/health check
     // Útil para verificar se o servidor está rodando
